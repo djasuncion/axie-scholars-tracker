3cf83b8ee395ebda2b035c4f62a4336c254ee308
DJ Asuncion <22920210+djasuncion@users.noreply.github.com>
2021-12-19 14:20:33 +0800

    installed firebase, initiated firebase proj and config file

---
+++ lib/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBvCW8O5Bmb4AMnhf77aDPlA8oLfSutuEg',
+  authDomain: 'axie-scholars-tracker.firebaseapp.com',
+  projectId: 'axie-scholars-tracker',
+  storageBucket: 'axie-scholars-tracker.appspot.com',
+  messagingSenderId: '929738943539',
+  appId: '1:929738943539:web:b521e797d61add858e0746',
+  measurementId: 'G-1B5JHNCV2W',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
